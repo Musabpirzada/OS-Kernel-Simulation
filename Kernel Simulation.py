10b2334d394c303685e28d7da25df61f8f53c113
Musab <musabpirzada1@gmail.com>
2023-02-09 15:33:58 +0500

    creation of main menu class

--- Kernel Simulation.py
+++ Kernel Simulation.py
@@ -6,5 +6,12 @@ screen_height = root.winfo_screenheight()
 # Set the window size to the screen size
 root.geometry(f"{screen_width}x{screen_height}")
 
-
+class Main_Menu:
+    def __init__(self, root):
+        self.blank_label = Label(root, text='', height=2)
+        self.blank_label.pack()
+        self.label_title = Label(root, text="OS Kernal Simulation", font="Times 36 italic bold")
+        self.label_title.pack()
+        self.blank_label1 = Label(root, text='', height=2)
+        self.blank_label1.pack()
 root.mainloop()
