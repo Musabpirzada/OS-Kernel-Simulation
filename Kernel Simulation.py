8c3e6793566a33dffae1e771185eeefadc4f0131
Musab <musabpirzada1@gmail.com>
2023-02-09 15:35:28 +0500

    Creation of Process Management class

--- Kernel Simulation.py
+++ Kernel Simulation.py
@@ -14,4 +14,14 @@ class Main_Menu:
         self.label_title.pack()
         self.blank_label1 = Label(root, text='', height=2)
         self.blank_label1.pack()
+
+class ProcessManagement:
+    def __init__(self):
+        self.blank_label = Label(root, text='', height=1)
+        self.blank_label.pack()
+
+        self.label_title = Label(root, text="Process Management log", font="Times 24 bold")
+        self.label_title.pack()
+        self.blank_label1 = Label(root, text='', height=2)
+        self.blank_label1.pack()
 root.mainloop()
