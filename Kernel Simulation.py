f7fabb95c814e1acfc227f4ffe637935a61b0800
Musab <musabpirzada1@gmail.com>
2023-02-09 15:08:13 +0500

    Added the screen size of tkinter windows

--- Kernel Simulation.py
+++ Kernel Simulation.py
@@ -1,6 +1,10 @@
 from tkinter import *
 root = Tk()
-root.title("FASTEffos")
+root.title("FASTEffos") #OS name
+screen_width = root.winfo_screenwidth()
+screen_height = root.winfo_screenheight()
+# Set the window size to the screen size
+root.geometry(f"{screen_width}x{screen_height}")
 
 
 root.mainloop()
