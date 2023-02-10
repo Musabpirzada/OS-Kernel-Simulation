b65b394d98f68f95bae5876c4784b0fb304d1cca
Musab <musabpirzada1@gmail.com>
2023-02-10 11:45:34 +0500

    Added I/O management button and class

--- Kernel Simulation.py
+++ Kernel Simulation.py
@@ -22,17 +22,26 @@ class Main_Menu:
         self.btn_Process_m = Button(root, text="Process Management", command=process_manage, fg="White", bg="Red")
         self.btn_Process_m.configure(width=50, height=2)
         self.btn_Process_m.pack()
-        
+
         def memory_manage():
             self.destroy_elements_main()
             mem_m = MemoryManagement()
 
         self.blank_label2 = Label(root, text='', height=1)
         self.blank_label2.pack()
-
         self.btn_memory_m = Button(root, text="Memory Management", command=memory_manage, fg="White", bg="Black")
         self.btn_memory_m.configure(width=50, height=2)
         self.btn_memory_m.pack()
+
+        def io_manage():
+            self.destroy_elements_main()
+            io_m = IO_Management()
+
+        self.blank_label3 = Label(root, text='', height=1)
+        self.blank_label3.pack()
+        self.btn_io_m = Button(root, text="I/O Management", command=io_manage, fg="White", bg="Blue")
+        self.btn_io_m.configure(width=50, height=2)
+        self.btn_io_m.pack()
 
 class ProcessManagement:
     def __init__(self):
@@ -50,4 +59,8 @@ class MemoryManagement:
         self.label_title.pack()
         self.blank_label1 = Label(root, text='', height=2)
         self.blank_label1.pack()
+class IO_Management:
+    def __init__(self):
+        self.manage_label = Label(root, text="I/O Management Block")
+        self.manage_label.pack()
 root.mainloop()
