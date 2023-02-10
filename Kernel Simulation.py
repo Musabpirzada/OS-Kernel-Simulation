7186a1e2a191f33e7e4224b59aeda61995afb72a
Musab <musabpirzada1@gmail.com>
2023-02-10 11:43:54 +0500

    Added Memory Management button and class

--- Kernel Simulation.py
+++ Kernel Simulation.py
@@ -22,13 +22,31 @@ class Main_Menu:
         self.btn_Process_m = Button(root, text="Process Management", command=process_manage, fg="White", bg="Red")
         self.btn_Process_m.configure(width=50, height=2)
         self.btn_Process_m.pack()
+        
+        def memory_manage():
+            self.destroy_elements_main()
+            mem_m = MemoryManagement()
+
+        self.blank_label2 = Label(root, text='', height=1)
+        self.blank_label2.pack()
+
+        self.btn_memory_m = Button(root, text="Memory Management", command=memory_manage, fg="White", bg="Black")
+        self.btn_memory_m.configure(width=50, height=2)
+        self.btn_memory_m.pack()
 
 class ProcessManagement:
     def __init__(self):
         self.blank_label = Label(root, text='', height=1)
         self.blank_label.pack()
-
         self.label_title = Label(root, text="Process Management log", font="Times 24 bold")
+        self.label_title.pack()
+        self.blank_label1 = Label(root, text='', height=2)
+        self.blank_label1.pack()
+class MemoryManagement:
+    def __init__(self):
+        self.blank_label2 = Label(root, text='', height=1)
+        self.blank_label2.pack()
+        self.label_title = Label(root, text="Memory Management Block", font="Times 24 bold")
         self.label_title.pack()
         self.blank_label1 = Label(root, text='', height=2)
         self.blank_label1.pack()
